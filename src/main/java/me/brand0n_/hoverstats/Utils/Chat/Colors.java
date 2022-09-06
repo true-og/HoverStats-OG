47751a2b1a67b836b1dda579d1c73ce61378b15d
Brand0n1 <ghostkiller256@gmail.com>
2022-09-06 14:46:59 -0700

    - Fixed issue where using '&r' would only result in the color being reset - Removed unnecessary code

--- src/main/java/me/brand0n_/hoverstats/Utils/Chat/Colors.java
+++ src/main/java/me/brand0n_/hoverstats/Utils/Chat/Colors.java
@@ -2,8 +2,6 @@ package me.brand0n_.hoverstats.Utils.Chat;
 
 import me.brand0n_.hoverstats.HoverStats;
 import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.TextComponent;
-import net.md_5.bungee.api.chat.hover.content.Text;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -29,27 +27,6 @@ public class Colors {
         }
         // Allow user to use & instead of the weird minecraft color codes
         return ChatColor.translateAlternateColorCodes('&', s);
-    }
-
-    // Text component chat color
-    public Text textCompChatColor(String str) {
-        Text textComp = new Text(TextComponent.fromLegacyText(chatColor(str)));
-
-        // Check if the server is 1.16
-        if (plugin.version.isCorrectVersionHex()) {
-            // Check if there is a hex in the string
-            Matcher match = pattern.matcher(str);
-
-            while (match.find()) {
-                String color = str.substring(match.start(), match.end());
-                ChatColor hexColor = ChatColor.of(color);
-                str = str.replace(color, hexColor + "");
-                textComp = new Text(str);
-                match = pattern.matcher(str);
-            }
-        }
-        // Allow user to use & instead of the weird minecraft color codes
-        return textComp;
     }
 
     // Get final color
--- src/main/java/me/brand0n_/hoverstats/Utils/Hover/HoverUtils.java
+++ src/main/java/me/brand0n_/hoverstats/Utils/Hover/HoverUtils.java
@@ -8,39 +8,32 @@ import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.chat.hover.content.Text;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HoverUtils {
     private static final HoverStats plugin = HoverStats.getPlugin(HoverStats.class); // Get this from main
 
     public TextComponent setupHoverMessage(Player p, String message) {
-        String hover = plugin.colors.chatColor(formatHoverMessage(p, plugin.variable.statsHover()));
-        Text hoverText = plugin.colors.textCompChatColor(hover);
-
         TextComponent mainComponent = new TextComponent(TextComponent.fromLegacyText(plugin.colors.chatColor(message)));
-
         mainComponent.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, plugin.colors.chatColor(plugin.variable.hoverCommand().replace("%player%", p.getName()))));
-        mainComponent.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, hoverText));
+        mainComponent.setHoverEvent(formatHoverMessage(p, plugin.variable.statsHover()));
         return mainComponent;
     }
 
-    private String formatHoverMessage(Player p, List<String> strList) {
-        StringBuilder output = new StringBuilder();
-        for (int i = 0; i < strList.size(); i++) {
-            String str = strList.get(i);
-            str = plugin.placeholders.addPlaceholders(p, str);
-            if (str.contains("{") && str.contains("}") && str.length() > 2) {
-                str = str.replace("{", "%").replace("}", "%");
-            }
+    private HoverEvent formatHoverMessage(Player p, List<String> strList) {
+        HoverEvent finalOutput = new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ArrayList<>());
+        
+        for (int i=0; i < strList.size(); i++) {
+            String output = strList.get(i);
+            output = output.replace("[", "").replace("]", "");
+            output = plugin.placeholders.addPlaceholders(p, output);
+            finalOutput.addContent(new Text(output));
             if (i < strList.size()-1) {
-                output.append(str.trim()).append("\n&r&f");
-                continue;
+                finalOutput.addContent(new Text("\n"));
             }
-            output.append(str.trim()).append("&r&f");
         }
-        if (output.toString().equalsIgnoreCase("null") || output.toString().isEmpty()) {
-            output.append(plugin.variable.placeholderPlaceholder());
-        }
-        return plugin.colors.chatColor(output.toString());
+
+        return finalOutput;
     }
 }
