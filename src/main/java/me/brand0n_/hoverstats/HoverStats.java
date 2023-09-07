25ca000c40ab370f0f7e1dafaad27b005226f7a9
Brand0n1 <ghostkiller256@gmail.com>
2023-09-07 11:24:46 -0700

    - Removed unused code
    Signed-off-by: Brand0n1 <ghostkiller256@gmail.com>

--- src/main/java/me/brand0n_/hoverstats/HoverStats.java
+++ src/main/java/me/brand0n_/hoverstats/HoverStats.java
@@ -14,8 +14,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class HoverStats extends JavaPlugin {
     // Spigot id
     public String resourceID = "100245";
-    // Config Version
-    public String configVersion = "5.5.6";
 
     // Plugin Instance
     public static HoverStats getInstance;
